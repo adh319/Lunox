f3d84e20d4aa6becaf1158da7fb67719e4abe388
Enour? <mail.adheridwan@gmail.com>
2023-01-28 14:46:15 +0800

    Update lunox.js

--- src/lunox.js
+++ src/lunox.js
@@ -14,11 +14,9 @@ class MainClient extends Client {
             },
             intents: [
                 GatewayIntentBits.Guilds,
-                GatewayIntentBits.GuildMembers,
                 GatewayIntentBits.GuildVoiceStates,
                 GatewayIntentBits.GuildMessages,
                 GatewayIntentBits.MessageContent,
-                GatewayIntentBits.GuildMessageReactions,
             ],
             partials: [Partials.Message, Partials.Channel, Partials.Reaction],
         });
