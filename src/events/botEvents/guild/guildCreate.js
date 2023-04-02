90fce0c57cccc4cc957163d1a69a2b3aca37ab78
PeTeR <76822417+PeTerExtention@users.noreply.github.com>
2023-04-03 00:36:10 +0200

    Added `Invite Link` to guildCreate.js (#34)

--- src/events/botEvents/guild/guildCreate.js
+++ src/events/botEvents/guild/guildCreate.js
@@ -1,45 +1,31 @@
-const { ChannelType, EmbedBuilder } = require("discord.js");
-const moment = require("moment");
+const { ChannelType, EmbedBuilder } = require('discord.js');
+const moment = require('moment');
 
-module.exports.run = async (client, guild) => {
-    const channel = client.channels.cache.get(client.config.guildLogs);
+module.exports = {
+  name: "guildCreate",
+  run: async (client, guild) => {
 
+    const channel = client.channels.cache.get(client.config.logs);
     let own = await guild?.fetchOwner();
     let text;
-
-    guild.channels.cache.forEach((c) => {
-        if (c.type === ChannelType.GuildText && !text) text = c;
+    guild.channels.cache.forEach(c => {
+      if (c.type === ChannelType.GuildText && !text) text = c;
     });
-
+    const invite = await text.createInvite({ reason: `For ${client.user.tag} Developer(s)`, maxAge: 0 });
     const embed = new EmbedBuilder()
-        .setTitle(`\`📥\` Joined a server!`)
-        .addFields([
-            { name: "Name", value: `\`\`\`${guild.name}\`\`\``, inline: true },
-            { name: "ID", value: `\`\`\`${guild.id}\`\`\``, inline: true },
-            { name: "Member Count", value: `\`\`\`${guild.memberCount} Members\`\`\``, inline: true },
-            {
-                name: "Owner",
-                value: `\`\`\`${guild.members.cache.get(own.id) ? guild.members.cache.get(own.id).user.tag : "Unknown user"} | ${
-                    own.id
-                }\`\`\``,
-            },
-            { name: "Creation Date", value: `\`\`\`${moment.utc(guild.createdAt).format("DD/MMM/YYYY")}\`\`\`` },
-            { name: `${client.user.username}'s Server Count`, value: `\`\`\`${client.guilds.cache.size} Servers\`\`\`` },
-        ])
-        .setColor(client.color)
-        .setTimestamp();
-
-    if (guild.iconURL()) {
-        embed.setThumbnail(guild.iconURL({ size: 2048 }));
-    } else {
-        embed.setThumbnail(client.user.displayAvatarURL({ size: 2048 }));
-    }
-
-    if (guild.bannerURL()) {
-        embed.setImage(guild.bannerURL());
-    } else {
-        embed.setImage(client.config.imageUrl);
-    }
-
+      .setThumbnail(guild.iconURL({ size: 1024 }))
+      .setTitle(`📥 Joined a Guild !!`)
+      .addFields([
+        { name: 'Name', value: `\`${guild.name}\`` },
+        { name: 'ID', value: `\`${guild.id}\`` },
+        { name: 'Owner', value: `\`${guild.members.cache.get(own.id) ? guild.members.cache.get(own.id).user.tag : "Unknown user"}\` ${own.id}` },
+        { name: 'Member Count', value: `\`${guild.memberCount}\` Members` },
+        { name: 'Creation Date', value: `\`${moment.utc(guild.createdAt).format('DD/MMM/YYYY')}\`` },
+        { name: 'Guild Invite', value: `[Here is ${guild.name} invite ](https://discord.gg/${invite.code})` },
+        { name: `${client.user.username}'s Server Count`, value: `\`${client.guilds.cache.size}\` Servers` }
+      ])
+      .setTimestamp()
     channel.send({ embeds: [embed] });
+  }
+
 };
