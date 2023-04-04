8ef4567ca77e977eeec9e967364710b8ef586432
Adhe Ridwan <mail.adheridwan@gmail.com>
2023-04-05 03:58:23 +0700

    Fixed `v2.0.6` Errors, My Bad Sorry 😅

--- src/events/botEvents/guild/messageCreate.js
+++ src/events/botEvents/guild/messageCreate.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder } = require("discord.js");
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const { supportUrl } = require("../../../settings/config.js");
 
 module.exports.run = async (client, message) => {
