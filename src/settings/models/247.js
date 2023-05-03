ac028cb9b5e910763b2905324453fc92659c1f16
cacaarmelita <cacaarmelita.id@gmail.com>
2023-05-03 09:24:31 -0500

    Update 247 system voice config

--- src/settings/models/247.js
+++ src/settings/models/247.js
@@ -4,7 +4,7 @@ const CreateReconnect = mongoose.Schema({
     guild: { type: String, required: true },
     text: { type: String, default: null },
     voice: { type: String, default: null },
-    time: { type: Number, default: null },
+    time: { type: Number, default: null }, // disable this if you set the premium to false
 });
 
 module.exports = mongoose.model("Reconnect", CreateReconnect);
