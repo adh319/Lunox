6cbe0872a9f31f78baa145fc44e31efa51340fea
Adhe Ridwan <mail.adheridwan@gmail.com>
2024-10-16 17:45:56 +0800

    Update autoplay.js

--- src/commands/interaction/setting/autoplay.js
+++ src/commands/interaction/setting/autoplay.js
@@ -30,12 +30,14 @@ module.exports = {
 
         if (autoplay) {
             client.data.delete("autoplay", player.guildId);
+
             embed.setDescription(`Autoplay mode is now \`disabled\``);
         } else {
             client.data.set("autoplay", player.guildId);
+
             embed.setDescription(`Autoplay mode is now \`enabled\``);
         }
-        
+
         return interaction.reply({ embeds: [embed], ephemeral: true });
     },
 };
