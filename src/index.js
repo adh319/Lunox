15ca1cf6ab91a42d152c21de08e8ee9f5011d8aa
wlfyzzdev <S033417@shcg.ac.uk>
2025-12-12 17:50:09 +0000

    Refactor token import from config settings

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const { ClusterManager } = require("discord-hybrid-sharding"); //imports the sharding manager
 require("dotenv").config({path: "./.env", quiet: true}); // loads the .env file
-const config = require("./settings/config.js")
+const {token} = require("./settings/config.js")
 
 const manager = new ClusterManager(`${__dirname}/clients/manager.js`, {
     totalShards: "auto", // you can set to every number you want but for save mode, use "auto" option
     shardsPerClusters: 2, // Default is 2, you can any bigger number you want
     totalClusters: "auto", // you can set to every number you want but for save mode, use "auto" option
     mode: "process", // you can also choose "worker"
-    token: process.env.TOKEN || config.token, // put token in .env file or put it in /src/settings/config.js
+    token: process.env.TOKEN || token, // put token in .env file or put it in /src/settings/config.js
 });
 
 manager.on("clusterCreate", (cluster) => console.log(`[INFO] Launched cluster ${cluster.id}`));
